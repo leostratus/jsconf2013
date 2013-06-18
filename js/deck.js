b6a20d6c193d7306ee29a361a153651482d9ba73
Angelina Fabbro <angelinafabbro@gmail.com>
2013-06-18 14:38:16 -0700

    Update deck to be tidier

--- js/deck.js
+++ js/deck.js
@@ -12,18 +12,19 @@
   // This shit because my Kensington presentation clicker wasn't programmable
   // Seriously I should have bought a Logitech
 
-  document.body.onclick = function(e) {
-    bespoke.prev();
-    return false;
-  }
+  // document.body.onclick = function(e) {
+  //   bespoke.prev();
+  //   return false;
+  // }
 
-  document.oncontextmenu = function (e) { 
-     bespoke.next();
-     return false;
-  };
+  // document.oncontextmenu = function (e) { 
+  //    bespoke.next();
+  //    return false;
+  // };
 
   // Since this talk was an early prototype of a slide deck framework I'm tweaking, it doesn't work well across all browsers. Poop. I know.
   var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+  console.log(is_chrome)
   if (!is_chrome) {
     document.getElementById('warning').className += "animate-in";
     content = document.getElementsByClassName('content');
@@ -31,6 +32,5 @@
       content[i].style.padding = "300px inherit";
       content[i].style.margin = "0 auto";
     };
-
   };
 })();
